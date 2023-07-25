6d8154c250c167dcdc5d38983f8d6f65c832d287
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-25 13:27:36 +0200

    Created User model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from sqlmodel import SQLModel, Field
 
 
-class Item(SQLModel, table=True):
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str
+    username: str = Field(unique=True, max_length=255)
+    email: str = Field(unique=True, max_length=255)
+    hashed_password: str
