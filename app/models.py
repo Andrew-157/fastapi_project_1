89d531875af3fa4c6d2cc419779355fafecde242
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-01 08:35:17 +0200

    Deleted previous project files to start over

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel, Field
-from .schemas import UserBase
-
-
-class User(UserBase, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    hashed_password: str
