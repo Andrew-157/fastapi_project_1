c641e1cf906f90e8fc742883e50bd44e7c6b8cd8
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-01 09:06:56 +0200

    Migrated to create user table

--- app/models.py
+++ app/models.py
@@ -3,5 +3,5 @@ from .schemas import UserBase
 
 
 class User(UserBase, table=True):
-    int: int | None = Field(default=None, primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     hashed_password: str
